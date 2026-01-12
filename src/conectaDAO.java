29671a49951c1def018ca0a7c4099235a6530d36
Rafael Oliveira Dos Santos <grm_rafael1596@hotmail.com>
2026-01-12 01:56:48 -0300

    Alteração e Adição das funções de conectar e desconectar e Alteração da Dependencia

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -1,33 +1,45 @@
 
+import com.mysql.cj.jdbc.DatabaseMetaData;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Adm
  */
 public class conectaDAO {
-    
-    public Connection connectDB(){
-        Connection conn = null;
-        
+
+    Connection conn = null;
+    public String url = "jdbc:mysql://LocalHost:3306/uc11";
+    public String user = "root";
+    public String password = "123456";
+
+    public Connection connectDB() {
+
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url, user, password);
+
+        } catch (ClassNotFoundException | SQLException erro) {
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
         }
         return conn;
     }
-    
+
+    public void desconnectDB() {
+        try {
+            conn.close();
+
+        } catch (SQLException e) {
+
+        }
+
+    }
+
 }
